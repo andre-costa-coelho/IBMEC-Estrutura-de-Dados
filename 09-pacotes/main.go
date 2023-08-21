e0fffd07978859b0c7ebf51724629a05950d221a
André Costa Coelho <andreccoelho2004@gmail.com>
2023-08-21 09:00:37 -0300

    Aula 9 - Pacotes

---
+++ 09-pacotes/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"projeto/utils"
+)
+func main(){
+	fmt.Println("Olá,mundo!")
+
+	fmt.Println(utils.Somar(6.4, 8.2))
+}
---
+++ 09-pacotes/utils/calculo.go
@@ -0,0 +1,9 @@
+package utils
+
+func Somar(a,b float64) float64{  //SEMPRE LETRA MAIÚSCULA.
+	return a + b
+}
+
+func Subtrair(a,b float64) float64{  //SEMPRE LETRA MAIÚSCULA.
+	return a - b
+}
