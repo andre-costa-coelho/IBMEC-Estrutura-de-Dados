38e558cea53cd738ecaa815942ed15e8193ca50e
André Costa Coelho <andreccoelho2004@gmail.com>
2023-11-04 14:11:14 -0300

    Update arvoreBinariaBusca.go

--- AC8/arvoreBinariaBusca.go
+++ AC8/arvoreBinariaBusca.go
@@ -3,82 +3,107 @@ package main
 import "fmt"
 
 type No struct {
-	Chave   int
+	Chave    int
 	Esquerda *No
-	Direita *No
+	Direita  *No
+	Altura   int
 }
 
 type Arvore struct {
-    Raiz *No
+	Raiz *No
 }
 
-
-func (t *Arvore) insereArvore(ch int) {
-    novoNo := &No{Chave: ch}
-
-    if t.Raiz == nil {
-        t.Raiz = novoNo
-    } else {
-        insereNo(t.Raiz, novoNo)
-    }
-}
-
-func insereNo(no, novoNo *No) {
-    if novoNo.Chave < no.Chave {
-        if no.Esquerda == nil {
-            no.Esquerda = novoNo
-        } else {
-            insereNo(no.Esquerda, novoNo)
-        }
-    } else {
-        if no.Direita == nil {
-            no.Direita = novoNo
-        } else {
-            insereNo(no.Direita, novoNo)
-        }
-    }
-}
-
-func (t *Arvore) buscaArvore(ch int) bool {
-	return buscaNo(t.Raiz, ch)
-}
-
-
-func buscaNo(no *No, ch int) bool {
-	if no == nil {
-		return false
+func imprimeArvore(a Arvore) {
+	if a.Raiz != nil {
+		imprimeSimetrico(a.Raiz)
 	}
+}
 
-	if ch < no.Chave {
-		return buscaNo(no.Esquerda, ch)
-	} else if ch > no.Chave {
-		return buscaNo(no.Direita, ch)
+func imprimeSimetrico(n *No) {
+	if n.Esquerda != nil {
+		imprimeSimetrico(n.Esquerda)
 	}
-
-	return true
+	fmt.Println(n.Chave)
+	if n.Direita != nil {
+		imprimeSimetrico(n.Direita)
+	}
 }
 
 func main() {
-    arvore := &Arvore{}
+	arv := Arvore{}
 
-    // Inserir elementos na árvore
-    chaves := []int{10, 5, 15, 3, 7, 12, 18}
-    for _, chave := range chaves {
-        arvore.insereArvore(chave)
-    }
+	fmt.Println("\nInserindo valores em uma árvore nula")
+	fmt.Println(insereArvore(&arv, 3))
+	imprimeArvore(arv)
 
-    // Verificar se um valor existe na árvore
-    buscaChave := 7
-    if arvore.buscaArvore(buscaChave) {
-        fmt.Printf("%d está na árvore.\n", buscaChave)
-    } else {
-        fmt.Printf("%d não está na árvore.\n", buscaChave)
-    }
+	fmt.Println("\nInserindo valores já existentes em uma árvore existente")
+	fmt.Println(insereArvore(&arv, 3))
+	imprimeArvore(arv)
 
-    buscaChave = 9
-    if arvore.buscaArvore(buscaChave) {
-        fmt.Printf("%d está na árvore.\n", buscaChave)
-    } else {
-        fmt.Printf("%d não está na árvore.\n", buscaChave)
-    }
+	fmt.Println("\nInserindo valores novos em uma árvore existente")
+	fmt.Println(insereArvore(&arv, 7))
+	fmt.Println(insereArvore(&arv, 1))
+	fmt.Println(insereArvore(&arv, 6))
+	fmt.Println(insereArvore(&arv, 5))
+	fmt.Println(insereArvore(&arv, 9))
+	fmt.Println(insereArvore(&arv, 2))
+	imprimeArvore(arv)
+}
+
+// buscaArvore: Busca a partir do nó raiz, o valor passado como parâmetro e retorna: 0 se não tiver árvore, 1 se o valor for encontrado, 2 ou 3 se o valor não for encontrado e indicar o lado da árvore que deve ser inserido
+func buscaArvore(opcao *int, no **No, valor int) {
+	if *no == nil {
+		*opcao = 0
+		return
+	}
+
+	if (*no).Chave == valor {
+		*opcao = 1
+		return
+	}
+
+	if valor < (*no).Chave {
+		if (*no).Esquerda == nil {
+			*opcao = 2
+			return
+		}
+
+		*no = (*no).Esquerda
+		buscaArvore(opcao, no, valor)
+	} else {
+		if (*no).Direita == nil {
+			*opcao = 3
+			return
+		}
+
+		*no = (*no).Direita
+		buscaArvore(opcao, no, valor)
+	}
+}
+
+func insereArvore(arvore *Arvore, valor int) string {
+	var opcao int
+	no := arvore.Raiz
+
+	buscaArvore(&opcao, &no, valor)
+
+	if opcao == 1 {
+		return "Valor já existe na árvore"
+	}
+
+	novoNo := &No{Chave: valor}
+	novoNo.Chave = valor
+	novoNo.Esquerda = nil
+	novoNo.Direita = nil
+
+	if opcao == 0 {
+		arvore.Raiz = novoNo
+		return "Valor inserido com sucesso"
+	} else if opcao == 2 {
+		no.Esquerda = novoNo
+		return "Valor inserido com sucesso"
+	} else {
+		no.Direita = novoNo
+		return "Valor inserido com sucesso"
+	}
 }
